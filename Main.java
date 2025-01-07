f470d88f7e8e4586e01ba2c0f38ddca80589a856
MatthewEVogel <156533668+MatthewEVogel@users.noreply.github.com>
2025-01-07 17:58:36 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -17,9 +19,9 @@ public class Main {
             String[] inputArray = input.split(" ");
             String command = inputArray[0];
             String argument = String.join(" ", Arrays.copyOfRange(inputArray, 1, inputArray.length));
-            List<String> builtInCommands = List.of("echo", "exit", "type");
+            List<String> builtInCommands = List.of("echo", "exit", "type", "pwd", "cd");
             String path = System.getenv("PATH");
-//            System.out.println(path);
+//            System.out.println("path: " + path);
             String[] paths = {};
             if (path != null) {
                 paths = path.split(":");
@@ -36,17 +38,59 @@ public class Main {
                         System.out.println(argument + " is a shell builtin");
                     }
                     else {
-                        String filePath = fileExists(paths, argument);
+                        String filePath = findFileInPath(paths, argument);
                         if (filePath != null){
                             System.out.println(argument + " is " + filePath);
                         }
                         else {
-                            System.out.println(input + ": command not found");
+                            System.out.println(argument + ": not found");
                         }
                     }
                     break;
+                case "pwd":
+                    System.out.println(System.getProperty("user.dir"));
+                    break;
+                case "cd":
+                    if (argument.charAt(0) == '~'){
+                        if (argument.length() == 1){
+                            argument = System.getenv("HOME");
+                        }
+                        else {
+                            argument = System.getenv("HOME") + argument.split("~")[0];
+                        }
+                    }
+                    File directory = new File(argument);
+                    String[] parentLevels = argument.split("/");
+                    boolean relativeDir = false;
+                    if (parentLevels[0].equals(".") || parentLevels[0].equals(".."))  {
+                        relativeDir = true;
+                        for (int i = 0; i < parentLevels.length; i++){
+                            String level = parentLevels[i];
+                            if (!level.equals("..")) {
+                                String dir = "";
+                                for (int j = i+1; j < parentLevels.length; j++){
+                                    dir = dir + "/" + parentLevels[j];
+                                }
+                                dir = System.getProperty("user.dir") + dir;
+                                System.setProperty("user.dir", dir);
+                                directory = new File(dir);
+                                break;
+                            }
+                            Path currentDirectory = Paths.get(System.getProperty("user.dir"));
+                            Path parentDirectory = currentDirectory.getParent();
+                            System.setProperty("user.dir", parentDirectory.toString());
+                        }
+                    }
+                    if (directory.exists() && directory.isDirectory() && !relativeDir) {
+                        System.setProperty("user.dir", argument);
+                    }
+                    else if (!relativeDir){
+                    System.out.println("cd: " + argument + ": No such file or directory");
+                }
+
+                    break;
                 default:
-                    String filePath = fileExists(paths, argument);
+                    String filePath = findFileInPath(paths, command);
                     if (filePath != null){
                         Process process = Runtime.getRuntime().exec(new String[] {filePath, argument});
                         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -54,24 +98,20 @@ public class Main {
                         while ((line = reader.readLine()) != null) {
                             System.out.println(line);
                         }
-
-                        // Wait for the process to complete and check the exit code
-                        int exitCode = process.waitFor();
-                        System.out.println("Process exited with code: " + exitCode);
                     }
-
-
-                    else {
+                    else{
                         System.out.println(input + ": command not found");
                     }
             }
         }
     }
-    public static String fileExists(String[] paths, String fileName){
+    public static String findFileInPath(String[] paths, String fileName){
+//        System.out.println(fileName);
         for (String p : paths){
-            File file = new File(p + "/" + fileName);
+            String filePath = p + "/" + fileName;
+            File file = new File(filePath);
             if (file.exists()){
-                return p + "/" + fileName;
+                return filePath;
             }
         }
         return null;
